65ac4cc9895dfe262902533136fecc149e2fa091
Andrew Murphy <andrew@andrewmurphy.io>
2024-12-15 15:04:41 +1100

    Add buy me a coffee

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+          <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="andrewmurphyio" data-description="Support me on Buy me a coffee!" data-message="Thanks for covering my AI costs!" data-color="#FF5F5F" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
